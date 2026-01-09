4c1685df95491eb2da5733053d6d149ddb43c10a
Alexander <alexanderzee11@gmail.com>
2026-01-09 14:44:32 +0100

    Update test_openai.py

--- ODOO AI/test_openai.py
+++ ODOO AI/test_openai.py
@@ -1,6 +1,6 @@
 from openai import OpenAI
 
-client = OpenAI(api_key="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")
+client = OpenAI(api_key="YOUR API KEY HERE")
 
 response = client.chat.completions.create(
     model="gpt-4o-mini",
@@ -8,3 +8,4 @@ response = client.chat.completions.create(
 )
 
 print(response.choices[0].message.content)
+
